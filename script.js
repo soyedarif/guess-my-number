2c1228bc7948adf02d041304d24fe151f20f8b2c
soyedarif <soyedariff@gmail.com>
2023-02-08 13:10:54 +0600

    break time

--- script.js
+++ script.js
@@ -9,15 +9,26 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value) */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = parseInt(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
+
+    //when there's no input
     if (!guess) { //if only takes true values but zero NAN falsy values in boolean so ! to make it true;
         document.querySelector('.message').textContent = '⛔ No Number!'
+
+
+        //when player wins
     } else if (guess === secretNumber) {
-        document.querySelector(".message").textContent = '🎉 Correct Number'
+        document.querySelector(".message").textContent = '🎉 Correct Number!';
+
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+
+        //when guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
 
@@ -28,6 +39,7 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = '💥 You lost the Game!';
             document.querySelector('.score').textContent = 0;
         }
+        //when guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
 
@@ -41,3 +53,8 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 })
 
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').
+})
