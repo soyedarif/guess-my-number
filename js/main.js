1438816df31adb8399185dcaf77b6f09ce049631
soyedarif <soyedariff@gmail.com>
2023-02-19 19:20:26 +0600

    refactored

--- js/main.js
+++ js/main.js
@@ -4,60 +4,44 @@ function getElementInnerValue(elementid) {
   const elementValue = document.getElementById(elementid).value;
   return parseInt(elementValue);
 }
-function getIdElement(elementid) {
+function htmlElementByID(elementid) {
   return document.getElementById(elementid);
 }
 
 document.getElementById('btn-check').addEventListener('click', function () {
-  const theScore = getIdElement('score');
-  theScore.innerText = score;
-  const theNumber = getIdElement('number');
   const guess = getElementInnerValue('guess');
-  const message = getIdElement('message');
-  const highScoreElement = getIdElement('highscore');
-  const highScore = parseInt(highScoreElement.innerText);
+  const message = htmlElementByID('message');
+  const scoreBoard = htmlElementByID('score');
+  const theNumber = htmlElementByID('number');
   if (!guess || isNaN(guess)) {
-    message.innerText = '⛔ No Number guessed...!!!';
-  } else if (guess > secretNumber) {
-    if (guess > 20) {
-      alert('⛔ Try Number Between 1-20');
-    } else {
-        message.innerText = '📈 Too High Try Again...!';
-      if (score > 1) {
-          score--;
-      } else {message.innerText = '💥 You lost the Game!'};
-    }
-  } else if (guess < secretNumber) {
-    if (guess < 0) {
-      alert("You can't guess negetive Number");
-    } else {
-        message.innerText = '📉 Too Low Try Again...!';
-        if (score > 1) {
-            score--;
-      } else {
-        message.innerText = '💥 You lost the Game!';
-      }
-    }
-  } else if (guess == secretNumber) {
+    message.innerText = '⛔ Try Number Between 1-20';
+    // when the player guesses right
+  } else if (guess === secretNumber) {
     theNumber.innerText = secretNumber;
     message.innerText = '🎉 Bravo You did it...!';
     document.body.style.backgroundColor = '#60b347';
     theNumber.style.width = '30rem';
-    if (score > highScore) {
-      highScoreElement.innerText = score;
-    }
-  }
+    //when the player guesses wrong
+  }else if(guess!==secretNumber){
+    if (score > 1) {
+      message.innerText = guess>secretNumber?'📈 Too High Try Again...!':'📉 Too Low Try Again...!';
+      score--;
+      scoreBoard.innerText = score;
+    } else {
+      message.innerText = '💥 You lost the Game';
+      scoreBoard.innerText = 0;
+  }}
 });
 
 document.getElementById('btn-again').addEventListener('click', function () {
   score = 20;
-  const theScore = getIdElement('score');
+  const theScore = htmlElementByID('score');
   theScore.innerText = score;
-  const theNumber = getIdElement('number');
+  const theNumber = htmlElementByID('number');
   theNumber.innerText = '?';
-  const guess = getIdElement('guess');
+  const guess = htmlElementByID('guess');
   guess.value = '';
-  const message = getIdElement('message');
+  const message = htmlElementByID('message');
   message.innerText = 'Start guessing...';
   document.body.style.backgroundColor = '#222';
   theNumber.style.width = '15rem';
